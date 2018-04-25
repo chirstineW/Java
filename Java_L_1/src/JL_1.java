1c3d09717970c558b6aaa898a20bc8c74e6fc414
christine_Wang <christine_Wang@asgard.com.tw>
2018-04-25 20:59:41 +0800

    [第一支java程式] by Christine Wang 我的第一支程式 上傳囉!!

---
+++ Java_L_1/src/JL_1.java
@@ -0,0 +1,31 @@
+
+public class JL_1 {
+    public static void main (String Args[])
+    {
+        String text;
+    	int length;
+    	int weidth;
+    	int height;
+    	float mp;
+    	float hp;
+    	
+    	text="主角受到怪物A的攻擊，造成8點傷害";
+    	length=20 ;
+    	weidth=5 ;
+    	height=10;
+    	mp=18.12f;
+    	hp=20.51f;
+    	
+    	System.out.println(text);
+    	text="主角的生命值剩餘";
+    	System.out.println(text+(mp-9.61));
+    	text="隊友A對主角使用瑪娜藥水回復了5點瑪娜值";
+    	System.out.println(text);
+    	System.out.println("主角的瑪娜值剩餘"+(hp+2.61));
+    	
+    	
+    	
+    	
+    	
+    }
+}
